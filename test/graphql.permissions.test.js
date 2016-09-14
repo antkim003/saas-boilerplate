0833fccdb433b4f3f8a6b8043de464a573c0f29f
John Hill <john@proofpositiveimaging.com>
2016-09-14 13:24:31 -0400

    mutate test works

--- test/graphql.permissions.test.js
+++ test/graphql.permissions.test.js
@@ -1,35 +1,83 @@
 /* eslint-disable no-undef */
 const promise = require('bluebird');
-// import {Permissions, Usertypes, users, userTypesAssignments} from './user_list';
+import {Permissions, Usertypes, users, userTypesAssignments} from './user_list';
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-
-// import {graphql} from 'graphql';
-// import Schema from '../src/server/graphql/rootSchema';
-// const server = require('.././src/server/server');
 
 const should = chai.should();// eslint-disable-line no-unused-vars
 
 chai.use(chaiHttp);
 
+let authToken = '';
+
 describe('Graphql route testing, permissions', () => {
+  // login to server inside beforeEach
   beforeEach(() => {
+    chai.request('http://localhost:3000')
+      .post('/graphql')
+      .send({
+        query: "query($email:Email!,$password:Password!){payload:login(email:$email, password:$password){user{id,email,isVerified},authToken}}",
+        variables: {email: "admin123@gmail.com", password: "password"}
+      })
+      .end((err, res) => {
+        authToken = res.body.data.payload.authToken;
+        if (err) {
+          console.log('Error inside before each ', err);
+        }
+      });
   });// end beforeEach
-// "{"query":"query($email:Email!,$password:Password!){payload:login(email:$email,password:$password){user{id,email,isVerified},authToken}}","variables":{"email":"admin123@gmail.com","password":"password"}}"
-  describe('Get All Users', () => {
-      it('it should get all the permissions', (done) => {
-          chai.request('http://localhost:3000')
-              .post('/graphql')
-              .send({query: "query($email:Email!,$password:Password!){payload:login(email:$email, password:$password){user{id,email,isVerified},authToken}}", variables: {email: "admin123@gmail.com", password: "password"}})
-              .end((err, res) => {
-                console.log(res.body.data.payload.authToken);
-                res.should.have.status(200);
-                if (err) {
-                  // console.log(err);
-                };
-                done();
-              });
+
+  describe('getAllPermissions', () => {
+    it('it should get all the permissions', done => {
+      chai.request('http://localhost:3000')
+        .post('/graphql')
+        .send({
+          query: "{getAllPermissions{id,name}}"
+        })
+        .end((err, res) => {
+          // console.log('res.body.data.getAllPermissions', res.body.data.getAllPermissions);
+          res.should.have.status(200);
+          res.body.data.getAllPermissions.length.should.equal(4);
+          res.body.data.getAllPermissions[1].name.should.equal('write');
+          res.body.data.getAllPermissions[2].id.should.equal(3);
+          if (err) console.log(err);
+          done();
         });
     });
+  });
+  describe('getPermissionById', () => {
+    it('it should get a permission by id', done => {
+      chai.request('http://localhost:3000')
+        .post('/graphql')
+        .send({
+          query: "query{getPermissionById(id:2){id,name}}"
+        })
+        .end((err, res) => {
+          res.should.have.status(200);
+          // console.log('res.body.data.getPermissionById', res.body.data.getPermissionById);
+          res.body.data.getPermissionById.name.should.equal('write');
+          res.body.data.getPermissionById.id.should.equal(2);
+          if (err) console.log(err);
+          done();
+        });
+    });
+  });
+  describe('createPermission', () => {
+    it('it should create a permission', done => {
+      chai.request('http://localhost:3000')
+        .post('/graphql')
+        .send({
+          query: 'mutation{createPermission(name:"fart"){id,name}}'
+        })
+        .end((err, res) => {
+          res.should.have.status(200);
+          console.log('res.body.data.createPermission', res.body.data);
+          // res.body.data.getPermissionById.name.should.equal('write');
+          // res.body.data.getPermissionById.id.should.equal(2);
+          if (err) console.log(err);
+          done();
+        });
+    });
+  });
 
 }); // end testing block
