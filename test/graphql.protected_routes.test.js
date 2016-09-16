4ca6aeda619a2de4267651a97f662bfec59e0fcb
antkim003@gmail.com <antkim003@gmail.com>
2016-09-16 14:16:01 -0400

    test fix

--- test/graphql.protected_routes.test.js
+++ test/graphql.protected_routes.test.js
@@ -8,27 +8,33 @@ const should = chai.should();// eslint-disable-line no-unused-vars
 chai.use(chaiHttp);
 
 describe('Graphql Permissions route testing', () => {
+  // auth token cache this is working like localStorage would on our clientside
   let authToken = '';
   before(done => {
-    seed().then(() => {
-      chai.request('http://localhost:3000')
-        .post('/graphql')
-        .send({
-          query: "query($email:Email!,$password:Password!){payload:login(email:$email, password:$password){user{id,email,isVerified},authToken}}",
-          variables: {email: "admin123@gmail.com", password: "password"}
-        })
-        .end((err, res) => {
-          authToken = res.body.data.payload.authToken;
-          if (err) {
-            console.log('Error inside before each ', err);
-            done();
-          }
+    seed().then(done, done);
+  });
+  // logging in here for authentication
+  beforeEach(done => {
+    chai.request('http://localhost:3000')
+      .post('/graphql')
+      .send({
+        query: "query($email:Email!,$password:Password!){payload:login(email:$email, password:$password){user{id,email,isVerified},authToken}}",
+        variables: {email: "admin123@gmail.com", password: "password"}
+      })
+      .end((err, res) => {
+        console.log('response came back: ', res.body.payload, 'error: ', err);
+        authToken = res.body.data.payload.authToken;
+        if (err) {
+          console.log('Error inside before each ', err);
           done();
-        });
-    })// end then
-    .catch(err => {
-      console.error(err);
-    });
+        }
+        done();
+      });
+  });
+  // logging out after each login.
+  afterEach(done => {
+    authToken = '';
+    done();
   });
   describe('getAllPermissions', () => {
     it('it should get all the permissions when passing JWT token', done => {
@@ -39,7 +45,6 @@ describe('Graphql Permissions route testing', () => {
           query: "{getAllPermissions{id,name}}"
         })
         .end((err, res) => {
-          // console.log('res.body.data.getAllPermissions', res.body.data.getAllPermissions);
           res.should.have.status(200);
           res.body.data.getAllPermissions.length.should.equal(4);
           res.body.data.getAllPermissions[1].name.should.equal('write');
