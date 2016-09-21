dbdfd99879c072f6b57f28e81586180a51f75b9a
John Hill <john@proofpositiveimaging.com>
2016-09-21 17:56:10 -0400

    tweaked colors

--- src/universal/components/Navigation/Navigation.js
+++ src/universal/components/Navigation/Navigation.js
@@ -4,7 +4,7 @@ import Paper from 'material-ui/Paper';
 import React, {PropTypes, Component} from 'react';
 import styles from './Navigation.css';
 import {Link} from 'react-router';
-import smallLogo from './../Navigation/logo-small.png';
+import logo from './../Navigation/logo.png';
 
 export default class Navigation extends Component {
   static propTypes = {
@@ -13,10 +13,10 @@ export default class Navigation extends Component {
 
   render() {
     return (
-      <Paper zDepth={0} className={styles.nav}>
+      <Paper zDepth={3} className={styles.nav}>
         <Link to="/" className={styles.brand}>
-          <img src={smallLogo} width="38" height="38" alt="React"/>
-          <span>React, Redux, Postgres, Graphql</span>
+          <img src={logo} height="38" alt="Macys Logo"/>
+          <span> CMS</span>
         </Link>
         <div className={styles.menuButtons}>
           <Link className={styles.buttonBuffer} to="/cms">
